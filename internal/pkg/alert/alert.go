aaf53e7775a69f67aa10717004eec39b3f42d7fd
Csaba Fekete <feketecsaba@gmail.ccom>
2024-12-31 21:45:59 +0100

    Fixed error language

--- internal/pkg/alert/alert.go
+++ internal/pkg/alert/alert.go
@@ -2,19 +2,22 @@ package alert
 
 import (
 	"errors"
+	"maps"
+	"slices"
+	"strings"
 )
 
 const (
 	ALERT_TYPE_EMAIL      = "email"
 	ALERT_TYPE_SLACK      = "slack"
-	ALETR_TYPE_PUSHOVER   = "pushover"
+	ALERT_TYPE_PUSHOVER   = "pushover"
 	ALERT_TYPE_CUSTOM_CMD = "command"
 )
 
 var validAlertTypes = map[string]bool{
 	ALERT_TYPE_EMAIL:      true,
 	ALERT_TYPE_SLACK:      true,
-	ALETR_TYPE_PUSHOVER:   true,
+	ALERT_TYPE_PUSHOVER:   true,
 	ALERT_TYPE_CUSTOM_CMD: true,
 }
 
@@ -40,13 +43,15 @@ func GetAlerter(alert Alert) IAlerter {
 		return &EmailAlerter{alert, &GomailSender{}}
 	case ALERT_TYPE_CUSTOM_CMD:
 		return &CmdAlerter{alert}
+	case ALERT_TYPE_PUSHOVER:
+		return &PushoverAlerter{alert, &LivePushoverSender{}}
 	}
 	return nil
 }
 
 func (a *Alert) Validate() (bool, error) {
 	if _, ok := validAlertTypes[a.Type]; !ok {
-		return false, errors.New("invalid alert type, must be one of: email, slack, pushover, command")
+		return false, errors.New("invalid alert channel, must be one of: " + strings.Join(slices.Collect(maps.Keys(validAlertTypes)), ", "))
 	}
 	return GetAlerter(*a).Validate()
 }
